918f22a24ca3167e2b4bba1f0798f45b1ba93d47
Giovana Morais <giovana.vmorais@gmail.com>
2022-02-26 08:52:56 -0300

    general folder organization

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # beat_detection_experiments
-:drum: trying beat detection algorithms on non-western music datasets 
-
-
-## candombe
-* some audios had their beat positions shifted by some factor
-* for others, just shifting didn't work
-* 
+:drum: trying beat detection algorithms on non-western music datasets
---
+++ baseline.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import time
+
+import essentia
+import essentia.standard as es
+import librosa
+import librosa.display
+# IMPORTANT: since TCN is not available on pip version of madmom (0.16)
+# you have to build the library from source (0.17)
+import madmom
+import matplotlib.pyplot as plt
+import mir_eval
+import numpy as np
+
+base_path = Path.cwd().parent
+experiments_path = base_path / "experiments_results/baselines"
+dataset_path = base_path.parent.parent / "datasets/candombe"
+SR = 44100
+
+def librosa_beats(audio):
+    bpm, beats = librosa.beat.beat_track(x, sr=SR, units="time")
+    return beats
+
+def essentia_beats(audio):
+    beats, confidence = es.BeatTrackerMultiFeature()(x)
+    return beats
+
+#refence for implementation https://github.com/CPJKU/madmom/issues/403
+def madmom_rnn_beats(audio):
+    beat_processor = madmom.features.beats.RNNBeatProcessor()
+    beat_decoder = madmom.features.beats.DBNBeatTrackingProcessor(beats_per_bar=[4], fps=100)
+    beats = beat_decoder(beat_processor(audio))
+    return beats
+
+def madmom_tcn_beats(audio):
+    beat_processor = madmom.features.beats.TCNBeatProcessor()
+    beat_decoder = madmom.features.beats.DBNBeatTrackingProcessor(beats_per_bar=[4], fps=100)
+    beats = beat_decoder(beat_processor(audio))
+    return beats
